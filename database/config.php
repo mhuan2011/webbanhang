08fd6ce2ee82011ac453ee2d6f4e6588fd143800
mhuan2011 <minhhuan1100@gmail.com>
2021-01-26 17:18:50 +0700

    first commit

--- database/config.php
+++ database/config.php
@@ -1,7 +1,12 @@
 <?php
-    define("HOST" , "localhost");
-    define("USERNAME", "root");
-    define("PASSWORD", "");
-    define("DATABASE", "banhang");
+    // define("HOST" , "localhost");
+    // define("USERNAME", "root");
+    // define("PASSWORD", "");
+    // define("DATABASE", "banhang");
+
+    define("HOST" , "remotemysql.com");
+    define("USERNAME", "Bz7iGAS5ez");
+    define("PASSWORD", "zqPA4rt5vg");
+    define("DATABASE", "Bz7iGAS5ez");
 
     
